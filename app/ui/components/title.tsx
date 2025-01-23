79ddee50324038b27df8f3a586dd309c60b7e8f0
mtgnorton-imac24 <15726204663@163.com>
2025-01-23 17:57:20 +0800

    fix(all): lint错误

--- app/ui/components/title.tsx
+++ app/ui/components/title.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Title() {
   return (
     <Link href="/home">
-      <h1 className="text-4xl text-heading font-bold">Mtg's Blog</h1>
+      <h1 className="text-4xl text-heading font-bold">Mtg&apos;s Blog</h1>
     </Link>
   );
 }
