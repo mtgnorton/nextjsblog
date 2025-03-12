c6f6c3cdc94f3c8646d7282e5b60c5404389b8df
mtgnorton-imac24 <15726204663@163.com>
2025-03-12 18:18:38 +0800

    feat(profile): 完善个人简介，添加联系方式邮箱链接

--- app/ui/components/profile.tsx
+++ app/ui/components/profile.tsx
@@ -3,8 +3,19 @@
 
 export default function Profile() {
   return (
-    <div className="flex  gap-4 py-3">
-        我是一名开发工程师,熟悉后端开发,了解k8s,了解前端
+    <div className="flex flex-col gap-4 py-3">
+      <div>
+      我是一名开发工程师,熟悉后端开发,了解k8s,了解前端
+      </div>
+       <div>
+       联系方式: &nbsp; &nbsp;
+        <a 
+          href="mailto:ma19921212@gmail.com" 
+          className="text-link border-b-2 border-b-underline hover:text-hover"
+        >
+        ma19921212@gmail.com
+        </a>
+       </div>
     </div>
   );
 }
