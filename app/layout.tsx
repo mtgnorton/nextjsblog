caf2dae571cc43e8bbb27758c8ad5a6df6593aa0
mtgnorton-imac24 <15726204663@163.com>
2025-03-12 17:46:58 +0800

    refactor:  将首页的访问方式由跳转到/home,改为直接在跟域名下访问

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { inter } from '@/app/ui/font';
 import { Metadata } from 'next';
 import { ThemeProvider } from '@/app/provider/theme';
 import '@/app/(blog)/blog/atom-one-dark.css';
-
+import Nav from "@/app/ui/components/nav";
+import Title from "@/app/ui/components/title";
+import Footer from "@/app/ui/components/footer";
 export const metadata: Metadata = {
   title: {
     template: '%s | Blog',
@@ -24,7 +26,25 @@ export default function RootLayout({
      
       <body className={`${inter.className} antialiased h-100 w-100 overflow-x-hidden`} >
         <ThemeProvider>
-          {children}
+        <div className="text-primary flex flex-col min-h-screen max-w-[calc(700px-2*env(safe-area-inset-left))] mx-auto p-5 ">
+       <div >
+       <Title />
+       </div>
+        {/* menu */}
+        <div className="mt-10 mb-5">
+          <Nav />
+          </div> 
+
+        {/* content */}
+        <div className="w-full">
+            {children}
+        </div>
+
+        {/* footer */}
+        <div className=" mt-auto flex items-center justify-center">
+            <Footer />
+        </div>
+    </div>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import { redirect } from 'next/navigation';
+import { Metadata } from 'next';
+import PostsComponents from "@/app/ui/components/blog-lists";
+import Profile from "@/app/ui/components/profile";
+import { getPosts } from "@/app/ui/components/blog-lists";
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
-export default function Page() {
-  redirect('/home');
+export default async function BlogPage() {
+  const { posts, totalPages } = await getPosts({ sort: 'desc', page: 1,limit:5 });
+  return <div className="">
+    <Profile />
+    <div className="mt-10">
+      <h3 className="text-2xl font-bold mb-4">Recent posts</h3>
+      <PostsComponents posts={posts} totalPages={totalPages} />
+    </div>
+  </div>;
 }
+
